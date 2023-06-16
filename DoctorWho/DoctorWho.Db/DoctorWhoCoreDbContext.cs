6fd067f7707d25d9776dc0c3051ddae8e9590480
Dima <dimaabujaber422000@gmail.com>
2023-06-16 21:03:13 +0300

    Create the data models for Doctor Who tables with all necessary keys, relationships, constraints and navigations. These models should exist in DoctorWho.Db

--- DoctorWho/DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho/DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -4,6 +4,13 @@ namespace DoctorWho.Db
 {
     public class DoctorWhoCoreDbContext : DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Companion> Companions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<EpisodeCompanion> EpisodeCompanions { get; set; }
+        public DbSet<EpisodeEnemy> EpisodeEnemies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
